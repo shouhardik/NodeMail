9ea0c0c2f4ee419edfcca3e2208ac6a45fc24581
Shouhardik Saha <shouhardiksaha@gmail.com>
2021-11-26 18:49:07 +0530

    mailer

---
+++ app.js
@@ -0,0 +1,24 @@
+const nodemailer=require('nodemailer')
+
+const transporter=nodemailer.createTransport({
+    service:"hotmail",
+    auth:{
+        user:"shomsaha77@outlook.com",
+        pass:"gsrhshw1"
+    }
+})
+
+const options={
+    from :"shomsaha77@outlook.com",
+    to:"shouhardiksaha@gmail.com",
+    subject:"Sending email with node.js",
+    text:"Hi! , this is an email from outlook to gmail"
+}
+
+transporter.sendMail(options,(err,info)=>{
+  if(err){
+      console.log(err);
+      return;
+  }
+  console.log("Sent :"+info.response);
+})
